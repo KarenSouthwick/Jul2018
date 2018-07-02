0a8600c6bc16fc4f6e680ccee82dcc083c5567e4
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-07-02 14:43:27 +0100

    basic test

--- Jul2018/BasicTest.cs
+++ Jul2018/BasicTest.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
 
 namespace Jul2018
 {
+    [TestFixture]
     public class BasicTest
     {
+        IWebDriver driver = new ChromeDriver();
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://qa-platform.authenticateis.com/Account/Logon");
+            driver.FindElement(By.Id("UserName")).SendKeys("User1508");
+        }
+
+        [OneTimeTearDown]
+        public void EndTest()
+        {
+            driver.Quit();
+        }
+
+        [Test, Order(1)]
+        public void SignUp()
+        {
+            driver.FindElement(By.LinkText("register / sign up")).Click();
+        }
     }
 }
