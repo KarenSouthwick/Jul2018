542d90bdc11e519d155f425e531d1a47228a7616
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-07-02 15:39:37 +0100

    copy / paste class

---
+++ Jul2018/SecondScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
+
+namespace Jul2018
+{
+    [TestFixture]
+    public class SecondScript
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://qa-platform.authenticateis.com/Account/Logon");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 974);
+            driver.FindElement(By.Id("UserName")).SendKeys("ruudhartke");
+            driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
+            driver.FindElement(By.Id("do-submit")).Click();
+        }
+
+        [OneTimeTearDown]
+        public void EndTest()
+        {
+            driver.Quit();
+        }
+
+        [Test, Order(1)]
+        public void LoggaOut()
+        {
+            driver.FindElement(By.CssSelector(".lock")).Click();
+        }
+    }
+}
